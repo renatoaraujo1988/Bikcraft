93dffcc306c6c64c85b2aaf36c12789a667a9a82
Renato Araujo <renato.araujo1988@gmail.com>
2022-11-15 11:15:02 -0300

    versao22

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 
 // botao.addEventListener("click", mostrar);
 
-
+//Ativar menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -20,3 +20,55 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//ativar itens orcamento
+
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true
+    }
+}
+parametros.forEach(ativarProduto);
+
+//perguntas 
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+galeria.forEach(eventosGaleria);
+
+//Animacao
+if (window.SimpleAnime) {
+new SimpleAnime();
+}
