97b151014b1f879da3e31a3c9ea24532a4288816
Renato Araujo <renato.araujo1988@gmail.com>
2022-11-16 22:15:32 -0300

    versao 27

--- js/formulario.js
+++ js/formulario.js
@@ -1,9 +1,9 @@
 const formulario = document.querySelector("form");
 function formularioEnviado(resposta) {
     if(resposta.ok) {
-        formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 5px; background: #f7f7f7;'><span style='color: #317A00 ;'>Mensagem enviada com SUCESSO!</span> Aguarde nosso contato. Voltar à <a href='./'>Página Inicial</a></p>"
+        formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 100px; border-radius: 5px; background: #f7f7f7;'><span style='color: #317A00 ;'>Mensagem enviada com SUCESSO!</span> Aguarde nosso contato. Voltar à <a class='botao' href='./'>Página Inicial</a></p>"
     } else {
-        formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 5px; background:#f7f7f7;><span style='color: #E00000 ;'>Ops... Houve um erro!</span> Favor nos enviar uma mensagem de e-mail para contato@renatoaraujo.fun</p>"
+        formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 100px; border-radius: 5px; background:#f7f7f7;><span style='color: #E00000 ;'>Ops... Houve um erro!</span> Favor nos enviar uma mensagem de e-mail para contato@renatoaraujo.fun</p>"
     }
 }
 function enviarFormulario(event) {
